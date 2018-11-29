8b962bbe2411f755e919480eb5a76c120375bf38
Zealot817 <234632198@qq.com>
2018-11-29 22:36:00 +0800

    Learn to use class Model

--- DjangoLearning/index/urls.py
+++ DjangoLearning/index/urls.py
@@ -3,9 +3,21 @@
 from django.urls import path, re_path
 from . import views
 urlpatterns = [
-    path('', views.index),
+    path('', views.index)
+
     # 添加带有字符类型、整型和slug的URL
     #path('<year>/<int:month>/<slug:day>', views.mydate),
     # 上面的年份等价于    path('<str:year>/<int:month>/<slug:day>', views.mydate)
-    re_path('(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2}).html', views.mydate)
+
+    #re_path('(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2}).html', views.mydate)
+    # 上面的正则表达式 运行时显示page not found 或许是书上的例子有问题？
+
+    #re_path('(?P<year>[0-9]{4}).html', views.myyear, name='myyear'),
+    #不是很明白这有啥实际应用，写个demo能用点心么
+
+    #re_path('(?P<year>[0-9]{4}).html', views.myyear_dict, {'month':'05'}, name='myyear_dict')
+    #这个也不知道干嘛的
+
+     #path('download.html', views.download)
+    #下载文件
 ]
--- DjangoLearning/index/views.py
+++ DjangoLearning/index/views.py
@@ -1,8 +1,27 @@
 # index的views.py
+from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+import csv
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    type_list = Product.objects.values('type').distinct()
+    name_list = Product.objects.values('name','type')
+    context = {'title': '首页', 'type_list': type_list, 'name_list': name_list}
+    return render(request, 'index.html',context=locals(), status=200)
 
 def mydate(request, year, month, day):
     return HttpResponse(str(year) +'/'+ str(month) +'/'+ str(day))
+
+def myyear(request, year):
+    return render(request, 'myyear.html')
+
+def myyear_dict(request, year, month):
+    return render(request, 'myyear_dict.html', {'month':month})
+
+def download(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="firstDownloadFile.csv"'
+    writer = csv.writer(response)
+    writer.writerow(['First row', 'A', 'B', 'C'])
+    return response
